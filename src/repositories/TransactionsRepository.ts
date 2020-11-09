2be330f1ca73d8e42a642f7ba0d8da030db8682b
Aky Natan <akynatan@outlook.com>
2020-11-09 01:45:19 -0300

    Resolvendo Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const sumIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc, currentValue) => acc + currentValue.value, 0);
+
+    const sumOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc, currentValue) => acc + currentValue.value, 0);
+
+    const balance = {
+      income: sumIncome,
+      outcome: sumOutcome,
+      total: sumIncome - sumOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
